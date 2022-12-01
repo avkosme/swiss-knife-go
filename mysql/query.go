4e6b10bd7ddfd412552f5f8cce3d2aa267caaf0f
Andrei Kostiuchenko <avkosme@gmail.com>
2022-12-01 06:13:21 +0300

    dev: Add mysql Creater

--- mysql/query.go
+++ mysql/query.go
@@ -13,6 +13,31 @@ type Finder interface {
 	FindByQuery(db *sql.DB, q string, limit, offset int, err error) []map[string]any
 }
 
+type Creater interface {
+	Create(db *sql.DB, q string, err error, args ...any)
+}
+
+// Create
+func Create(db *sql.DB, q string, err error, args ...any) {
+	defer func() {
+		if r := recover(); r != nil {
+			erl("", err)
+			defer sentry.Flush(2 * time.Second)
+			sentry.CaptureException(err)
+		}
+	}()
+	defer db.Close()
+
+	// layout := "2006-01-02 15:04:05"
+	// createdAt := time.Now().Format(layout)
+	// updatedAt := &createdAt
+	_, err = db.Exec(
+		q,
+		args...,
+	)
+	erl("", err)
+}
+
 // FindByQuery
 func FindByQuery(db *sql.DB, q string, limit, offset int, err error) (result []map[string]any) {
 	if limit > 0 {
